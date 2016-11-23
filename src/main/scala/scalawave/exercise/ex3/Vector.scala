415edc7ee238185e9f36e8fa2e3bbcfe00499616
Jakub Kozłowski <kubukoz@gmail.com>
2016-11-23 16:48:02 +0100

    Add solution for ex3

--- src/main/scala/scalawave/exercise/ex3/Vector.scala
+++ src/main/scala/scalawave/exercise/ex3/Vector.scala
@@ -26,20 +26,38 @@ package scalawave.exercise.ex3
     in this task, but our own defined in ex1
 */
 
-import shapeless.ops.hlist.ToList
-import shapeless.{::, Generic, HList, HNil}
 
 import scalawave.common.TypeLevelNat._
 import scalawave.exercise.ex1._
+/*
+
+trait ToList[-T <: HList, +Lub]{
+  def toList(l: T): List[Lub]
+}
+object ToList {
+  implicit val hnilList: ToList[HNil, Nothing] = (_: HNil) => Nil
+
+  implicit def hconsList[Lub, H <: Lub, T <: HList]
+  (implicit tailList: ToList[T, Lub]): ToList[H :: T, Lub] =
+
+    (l: H :: T) => l.head :: tailList.toList(l.tail)
+}
+*/
+
 
 case class Vector[N <: Nat, +T](toList: List[T]) {
 
-  // TODO: copy your solution from ex2
+  def ::[U >: T](elem: U): Vector[Succ[N], U] = Vector(elem :: toList)
 
-  def ::[U >: T](elem: U): Vector[Succ[N], U] = ???
-  def ++[M <: Nat, U >: T](suffix: Vector[M, U]) = ???
-  def head: T = ???
-  def at[K <: Nat]: T = ???
+  def ++[M <: Nat, U >: T](suffix: Vector[M, U])
+                          (implicit add: Add[N, M]): Vector[add.Result, U] =
+    Vector(toList ::: suffix.toList)
+
+  // should not compile for empty vectors
+  def head(implicit nz: Zero Lt N): T = toList.head
+
+  // should not compile if index K is out of bound given by M
+  def at[K <: Nat](implicit lt: K Lt N, ti: ToInt[K]): T = toList(ti.value)
 
   override def toString: String =
     s"Vector[${toList.size}](${toList.mkString(", ")})"
@@ -47,7 +65,36 @@ case class Vector[N <: Nat, +T](toList: List[T]) {
 
 object VNil extends Vector[Zero, Nothing](Nil)
 
+import shapeless.{::, Generic, HList, HNil}
+
 object Vector {
 
-  def apply[T]() = ???
+  def apply() = VNil
+
+  def apply[T](t: T): Vector[Succ[Zero], T] = t :: VNil
+  import shapeless.ops.hlist._
+
+  def apply[P <: Product, L <: HList, T](elem: P)
+                         (implicit gen: Generic.Aux[P, L],
+                          length: HlistLength[L],
+                          toList: ToList[L, T])
+    : Vector[length.Len, T] = {
+
+    Vector(gen.to(elem).toList)
+  }
+}
+
+trait HlistLength[T <: HList] {
+  type Len <: Nat
+}
+
+object HlistLength {
+  implicit val hNilLength: HlistLength[HNil] = new HlistLength[HNil] {
+    override type Len = Zero
+  }
+
+  implicit def hListLength[H, Rest <: HList](implicit len: HlistLength[Rest]): HlistLength[H :: Rest] =
+    new HlistLength[::[H, Rest]] {
+      override type Len = Succ[len.Len]
+    }
 }
